6a6c12eb4d4fdeca460b0ea8ea535864bd5aad4d
Debjit <debjit.chongdar@gmail.com>
2025-06-25 15:12:30 +0530

    Window function use

--- src/main/java/com/spark/feature/datasetImpl/DatasetPivotTable.java
+++ src/main/java/com/spark/feature/datasetImpl/DatasetPivotTable.java
@@ -1,9 +1,86 @@
 package com.spark.feature.datasetImpl;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.functions;
+
+import static org.apache.spark.sql.functions.col;
 
 public class DatasetPivotTable {
     private SparkSession session;
@@ -23,5 +100,11 @@ public class DatasetPivotTable {
                 functions.avg("sal").as("avg_sal"),functions.avg("age").as("avg_age")
         );
         avgSalAndAgeOnDeptAndRating.show();
+
+        //Window function
+        Dataset<Row> deptWiseMaxSal = empDs.withColumn("row_num", functions.row_number().over(
+                Window.partitionBy("dept_id").orderBy(col("sal").desc())))
+                .filter(col("row_num").equalTo(1)).drop("row_num");
+        deptWiseMaxSal.show();
     }
 }
