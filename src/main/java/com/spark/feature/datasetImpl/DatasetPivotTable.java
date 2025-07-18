c63a144f672aa008ac80e5dacfef9da96f54e4a4
Debjit <debjit.chongdar@gmail.com>
2025-07-18 20:42:59 +0530

    order by and rank denseRank rownum

--- src/main/java/com/spark/feature/datasetImpl/DatasetPivotTable.java
+++ src/main/java/com/spark/feature/datasetImpl/DatasetPivotTable.java
@@ -4,6 +4,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
+import org.apache.spark.sql.expressions.WindowSpec;
 import org.apache.spark.sql.functions;
 
 import static org.apache.spark.sql.functions.col;
@@ -20,17 +21,26 @@ public class DatasetPivotTable {
                 functions.avg("sal").as("avg_sal"), functions.avg("age").as("avg_age")
         );
         groupedDs.show();
-        Dataset<Row> avgSalOnDeptAndRating = empDs.groupBy("dept_id").pivot("rating").agg(functions.avg("sal"));
+        Dataset<Row> avgSalOnDeptAndRating = empDs.groupBy("dept_id").pivot("rating")
+                .agg(functions.avg("sal").as("avg_sal"));
         avgSalOnDeptAndRating.show();
-        Dataset<Row> avgSalAndAgeOnDeptAndRating = empDs.groupBy("dept_id").pivot("rating").agg(
-                functions.avg("sal").as("avg_sal"),functions.avg("age").as("avg_age")
-        );
+        Dataset<Row> avgSalAndAgeOnDeptAndRating = empDs.groupBy("dept_id").pivot("rating")
+                .agg(functions.avg("sal").as("avg_sal"),functions.avg("age").as("avg_age"));
         avgSalAndAgeOnDeptAndRating.show();
 
         //Window function
+        WindowSpec partitionByOrder = Window.partitionBy("dept_id").orderBy(col("sal").desc());
+        Dataset<Row> allRank = empDs
+                .withColumn("row_number", functions.row_number().over(partitionByOrder))
+                .withColumn("dense_rank", functions.dense_rank().over(partitionByOrder))
+                .withColumn("rank", functions.rank().over(partitionByOrder));
+        allRank.show();
         Dataset<Row> deptWiseMaxSal = empDs.withColumn("row_num", functions.row_number().over(
                 Window.partitionBy("dept_id").orderBy(col("sal").desc())))
                 .filter(col("row_num").equalTo(1)).drop("row_num");
         deptWiseMaxSal.show();
+
+        empDs.orderBy(col("sal").desc()).show();
+        //empDs.select(col("name"), col("sal").desc()).show(); // It will not work one col descending in select clause
     }
 }
